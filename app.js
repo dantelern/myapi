57e34f56c205aeb35dfe76a19067cba41f4718ee
dantelern <110490913+dantelern@users.noreply.github.com>
2024-07-03 08:15:38 -0300

    Api basica

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+let users = [];
+
+app.get('/users', (req, res) => {
+  res.json(users);
+});
+
+app.post('/users', (req, res) => {
+  const user = { id: users.length + 1, ...req.body };
+  users.push(user);
+  res.status(201).json(user);
+});
+
+app.get('/users/:id', (req, res) => {
+  const user = users.find(u => u.id === parseInt(req.params.id));
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).send('Usuario no encontrado');
+  }
+});
+
+app.put('/users/:id', (req, res) => {
+  const user = users.find(u => u.id === parseInt(req.params.id));
+  if (user) {
+    Object.assign(user, req.body);
+    res.json(user);
+  } else {
+    res.status(404).send('Usuario no encontrado');
+  }
+});
+
+app.delete('/users/:id', (req, res) => {
+  const index = users.findIndex(u => u.id === parseInt(req.params.id));
+  if (index !== -1) {
+    users.splice(index, 1);
+    res.status(204).send();
+  } else {
+    res.status(404).send('Usuario no encontrado');
+  }
+});
+
+app.listen(3000, () => {
+  console.log('API corriendo en http://localhost:3000');
+});
